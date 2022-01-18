07f6ffaddcda76bbec715004c64002c2c5ecfa9b
Leonardo Dias Lissone Santomero <leonardo.lissonez@gmail.com>
2022-01-18 19:48:00 -0300

    feat: home page styled

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,15 @@
 import Head from 'next/head'
 
-import { Container } from '@styles/home'
+import {
+  Container,
+  ImageContainer,
+  Content,
+  Column,
+  Heading,
+  ResumeText,
+  HighlightText,
+  ContactButton
+} from '@styles/home'
 
 export default function Home() {
   return (
@@ -10,7 +19,48 @@ export default function Home() {
       </Head>
 
       <Container>
-        <h1>Home</h1>
+        <Content>
+          <img src="/images/cable-led.svg" alt="Imagem de cabo de led" />
+
+          <Column>
+            <Heading>
+              <h3>Olá,</h3>
+              <h2>Eu sou</h2>
+              <h1>Leonardo Lissone</h1>
+            </Heading>
+
+            <ResumeText>
+              <p>
+                Atualmente fazendo estágio na área de <strong>desenvolvimento</strong> na{' '}
+                <HighlightText>Serget Mobilidade Viária</HighlightText>, atuando
+                principalmente na manutenção e desenvolvimento de novas ferramentas, e
+                funcionalidades para usuários dos sistemas públicos e privados da empresa.
+              </p>
+
+              <p>
+                Além de estar muito <strong>determinado</strong> para meu{' '}
+                <strong>crescimento pessoal</strong> de conhecimento na área da
+                tecnologia, criando projetos sempre inovadores para mim com muito{' '}
+                <strong>amor</strong> e <strong>dedicação</strong>.
+              </p>
+            </ResumeText>
+
+            <ContactButton
+              to="contact"
+              smooth
+              duration={1000}
+              spy
+              exact="true"
+              offset={-80}
+            >
+              Contactar agora
+            </ContactButton>
+          </Column>
+        </Content>
+
+        <ImageContainer>
+          <img src="/images/bearing-led.svg" alt="Imagem de rolamento com led" />
+        </ImageContainer>
       </Container>
     </>
   )
--- src/styles/home.ts
+++ src/styles/home.ts
@@ -1,9 +1,112 @@
+import { Link as Scroll } from 'react-scroll'
 import styled from 'styled-components'
 
-export const Container = styled.main`
-  height: 100vh;
+export const Container = styled.div`
+  width: 100%;
+  height: calc(100vh - 5.625rem);
+  padding-left: 4rem;
+
+  display: flex;
+  justify-content: space-between;
+`
+
+export const ImageContainer = styled.div`
+  display: flex;
+  align-items: center;
+
+  img {
+    height: 38rem;
+  }
+`
+
+export const Content = styled.div`
+  padding-top: 6rem;
+
+  display: flex;
+  align-items: flex-start;
+
+  img {
+    margin-top: 6.2rem;
+    margin-right: 1rem;
+  }
+`
+
+export const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`
+
+export const Heading = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  h3 {
+    font-size: 2rem;
+    font-weight: 200;
+
+    text-transform: uppercase;
+    line-height: 2.5rem;
+
+    color: var(--gray-300);
+  }
+
+  h2 {
+    font-size: 2.5rem;
+    font-weight: 400;
+
+    text-transform: uppercase;
+    line-height: 2.5rem;
+
+    color: var(--gray-100);
+  }
 
   h1 {
-    font-size: 2rem;
+    font-size: 3rem;
+    font-weight: 700;
+
+    text-transform: uppercase;
+    line-height: 3.5rem;
+    border: 1px solid var(--gray-900-opacity-80);
+    text-shadow: 0px 4px 40px rgba(202, 62, 71, 0.5);
+
+    color: var(--red-400);
+  }
+`
+
+export const ResumeText = styled.div`
+  max-width: 56rem;
+  padding-top: 1rem;
+
+  display: flex;
+  flex-direction: column;
+  gap: 0.8rem;
+
+  p {
+    font-size: 1.2rem;
+  }
+`
+
+export const HighlightText = styled.span`
+  color: var(--red-200);
+`
+
+export const ContactButton = styled(Scroll)`
+  margin-top: 4rem;
+  padding: 0.8rem 4.2rem;
+
+  font-size: 1.2rem;
+
+  border: 1px solid var(--red-400);
+  border-radius: 0.5rem;
+  box-shadow: 0px 0px 30px rgba(202, 62, 71, 0.7);
+
+  color: var(--gray-300);
+  background: var(--gray-900-opacity-80);
+
+  transition: 0.3s transform;
+
+  &:hover {
+    transform: scale(1.03);
   }
 `
