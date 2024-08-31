0d50d0027969c2e4b7634f70497359df32dc9279
Lissone <leonardo.lissonez@gmail.com>
2024-08-31 18:36:48 -0300

    fix(sections/WorkExperiences): dynamic height for section

--- src/components/sections/WorkExperiences/styles.ts
+++ src/components/sections/WorkExperiences/styles.ts
@@ -2,7 +2,8 @@ import styled from 'styled-components';
 
 export const Container = styled.section`
   width: 100%;
-  height: 100vh;
+  min-height: 100vh;
+  height: 100%;
 
   display: flex;
   justify-content: space-between;
