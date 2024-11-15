a9cf479053ac39579564f684cfbc0d7f3050437e
Lissone <leonardo.lissonez@gmail.com>
2024-11-15 13:47:46 -0300

    style(components/sections/Projects/CategoryFilterModal): improve checkbox hover animation

--- src/components/sections/Projects/CategoryFilterModal/styles.ts
+++ src/components/sections/Projects/CategoryFilterModal/styles.ts
@@ -47,9 +47,10 @@ export const Content = styled.div`
   }
 
   input[type='checkbox']:hover + label {
-    filter: brightness(0.9);
+    border-color: var(--gray-600);
+    color: var(--gray-500);
 
-    transition: filter 0.2s;
+    transition: all 0.2s;
   }
 
   input[type='checkbox']:checked + label {
