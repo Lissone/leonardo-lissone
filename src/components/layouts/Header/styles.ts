b3e3ab3e8803a25c5aee2b218680cf6b2e7bc554
Lissone <leonardo.lissonez@gmail.com>
2024-11-15 13:34:36 -0300

    refactor(components/layouts/Header): remove transformation style to sidebar

--- src/components/layouts/Header/styles.ts
+++ src/components/layouts/Header/styles.ts
@@ -55,30 +55,7 @@ export const Menu = styled.div`
   justify-content: space-between;
 
   @media (max-width: 920px) {
-    width: 18rem;
-    height: 100vh;
-    padding-top: 1.5rem;
-
-    position: fixed;
-    z-index: 15;
-
-    top: 0rem;
-    left: 0.65rem;
-
-    opacity: 0%;
-    overflow: hidden;
-
-    flex-direction: column;
-    justify-content: flex-start;
-
-    background: var(--gray-800);
-    box-shadow: 0rem 0rem 1.875rem rgba(0, 0, 0, 0.25);
-
-    -webkit-font-smoothing: antialiased;
-    transform-origin: 0% 0%;
-    transform: translate(-110%, 0);
-
-    transition: all 0.5s cubic-bezier(0.77, 0.2, 0.05, 1);
+    display: none;
   }
 `;
 
@@ -111,10 +88,6 @@ export const LanguageMobileSwitchContainer = styled.nav`
 
   display: flex;
   justify-content: center;
-  
-  @media (max-width: 920px) {
-    margin-top: 1.5rem;
-  }
 `;
 
 export const Navigation = styled.nav`
@@ -123,15 +96,6 @@ export const Navigation = styled.nav`
   display: flex;
   align-items: center;
   gap: 3rem;
-  
-  @media (max-width: 920px) {
-    margin-top: 3rem;
-    padding: 0 2rem;
-
-    align-items: flex-start;
-    justify-content: center;
-    flex-direction: column;
-  }
 `;
 
 export const Buttons = styled.div`
@@ -139,13 +103,6 @@ export const Buttons = styled.div`
   align-items: center;
   justify-content: flex-end;
   gap: 2rem;
-
-  @media (max-width: 920px) {
-    margin-top: auto;
-    padding-bottom: 3rem;
-
-    flex-direction: column;
-  }
 `;
 
 export const ResumeButton = styled.a`
@@ -170,9 +127,5 @@ export const ResumeButton = styled.a`
     transform: scale(1.03);
 
     color: var(--red-400);
-  }
-
-  @media (max-width: 920px) {
-    font-size: 1.3rem;
   }
 `;
